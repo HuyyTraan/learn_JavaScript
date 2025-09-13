77299ac314e704f9d1dcb1735c06edb680bc95c2
HuyyTraan <tranhuy24022005@gmail.com>
2025-09-13 22:07:16 +0700

    update

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,6 +1,17 @@
 
-var fullName = 'Tran Vu Dinh Huy';
-var age = 20;
+// var fullName = 'Tran Vu Dinh Huy';
+// var age = 20;
 
-alert(fullName);
-alert(age);
+// alert(fullName);
+// alert(age);
+
+
+function writeLog() {
+    var myString = '';
+    for (var param of arguments) {
+        myString += `${param} - `;
+    }
+    console.log(myString);
+}
+
+writeLog('log1', 'log2', 'log3', 'log4');
