928844f9fe6ed81ac51912104a3eea23d6d5c874
HuyyTraan <tranhuy24022005@gmail.com>
2025-09-18 22:51:34 +0700

    update18/9

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -29,3 +29,81 @@ function User(name, age, address) {
 }
 
 var user = new User('Huy', 20, 'HCM');
+
+
+// Làm bài tại đây
+function Student(firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.getFullName = function(){
+        return firstName + ' ' + lastName;
+    }
+}
+
+Student.prototype.getFullName = function(){
+    return this.getFullName;
+}
+
+
+
+// Ví dụ khi sử dụng
+var student = new Student('Long', 'Bui');
+
+console.log(student.firstName);  // 'Long'
+console.log(student.lastName);  // 'Bui'
+console.log(student.getFullName());  // 'Long Bui'
+
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+
+var sports = [
+    {
+        name: 'Bơi lội',
+        gold: 11
+    },
+    {
+        name: 'Boxing',
+        gold: 3
+    },
+    {
+        name: 'Đạp xe',
+        gold: 4
+    },
+    {
+        name: 'Đấu kiếm',
+        gold: 5
+    },
+]
+function total(togold, sport){
+    togold += sport.gold;
+    return togold;
+}
+
+function getTotalGold(sport){
+    var final = sport.reduce(total, 0);
+    return final
+}
+
+
+// Expected results:
+console.log(getTotalGold(sports)) // Output: 23
+
+
+// function goldHandler(defauteValue, course) {
+//     defauteValue +=  course.gold;
+//     return defauteValue;
+// }
+
+// function getTotalGold(sports) {
+//    var value =  sports.reduce(goldHandler, 0);
+//    return value;
+// }
+
+// // Expected results:
+// console.log(getTotalGold(sports)) // Output: 23
+
+
+
+
